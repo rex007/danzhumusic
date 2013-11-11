4cb2323cb1f182db009e1971cc41d8cfd7503881
Andraz Kuret <rex007@gmail.com>
2013-11-11 22:00:54 +0100

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,37 +17,37 @@ set :scm, :git
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
 
-namespace :deploy do
+# namespace :deploy do
 
-  %w[start stop restart].each do |command|
-    desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
-    end
-  end
+#   %w[start stop restart].each do |command|
+#     desc "#{command} unicorn server"
+#     task command, roles: :app, except: {no_release: true} do
+#       run "/etc/init.d/unicorn_#{application} #{command}"
+#     end
+#   end
 
-  task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/site-enabled"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_"
-    run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
-    puts 'Now edit the config files in #{shared_path}'
-  end
-  after "deploy:setup", "deploy:setup_config"
+#   task :setup_config, roles: :app do
+#     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/site-enabled"
+#     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_"
+#     run "mkdir -p #{shared_path}/config"
+#     put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
+#     puts 'Now edit the config files in #{shared_path}'
+#   end
+#   after "deploy:setup", "deploy:setup_config"
 
-  task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  end
-  after "deploy:finalize_update", "deploy:symlink_config"
+#   task :symlink_config, roles: :app do
+#     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+#   end
+#   after "deploy:finalize_update", "deploy:symlink_config"
 
-  desc "Make sure local git is in sync with remote"
-  task :ckeck_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
-      puts "Run `git push` to sync changes"
-      exit
-    end
-  end
-  before "deploy", "deploy:check_revision"
-end
+#   desc "Make sure local git is in sync with remote"
+#   task :ckeck_revision, roles: :web do
+#     unless `git rev-parse HEAD` == `git rev-parse origin/master`
+#       puts "WARNING: HEAD is not the same as origin/master"
+#       puts "Run `git push` to sync changes"
+#       exit
+#     end
+#   end
+#   before "deploy", "deploy:check_revision"
+# end
 
